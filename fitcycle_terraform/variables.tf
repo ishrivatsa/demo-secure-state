d100d5497398cffb98428108af657ba3c422249d
Shrivatsa Upadhye <ushrivatsa@gmail.com>
2019-10-21 15:03:44 -0700

    Update variables.tf

--- fitcycle_terraform/variables.tf
+++ fitcycle_terraform/variables.tf
@@ -61,29 +61,3 @@ variable "organization" {
 
 variable "costcenter" {
 }
-
-    web="ami-0de53d8956e8dcf80"
-    mgmt="ami-0a313d6098716f372"
-    dblb="ami-0de53d8956e8dcf80"
-    db="ami-0a313d6098716f372"
-    app="ami-0de53d8956e8dcf80"
-    api="ami-0a313d6098716f372"
-   }
-}
-
-
-variable "option_3_aws_vpc_name" {}
-variable "option_4_aws_vpc_cidr" {}
-variable "option_5_aws_ssh_key_name" {}
-variable "option_6_aws_ssh_key_name" {}
-
-variable "option_9_use_rds_database" {}
-variable "option_10_aws_rds_identifier" {}
-variable "option_11_multi_az_rds" {}
-
-variable "product" {}
-variable "team" {}
-variable "owner" {}
-variable "environment" {}
-variable "organization" {}
-variable "costcenter" {}
